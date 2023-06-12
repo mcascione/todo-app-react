f803da5f8bd173355a2c76d8d2d02bf4ed3a48b6
Miranda Cascione <miranda.cascione@gmail.com>
2023-06-12 14:17:04 -0700

    added update/edit  todo functionality

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -14,6 +14,15 @@ function TodoList() {
     setTodos(newTodos);
   };
 
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
   const removeTodo = (id) => {
     const removeArr = [...todos].filter((todo) => todo.id !== id);
 
@@ -34,7 +43,12 @@ function TodoList() {
     <div>
       <h1>What's on the agenda for today?</h1>
       <TodoForm onSubmit={addTodo} />
-      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
     </div>
   );
 }
