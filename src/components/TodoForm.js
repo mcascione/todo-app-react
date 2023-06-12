893363238e4850fb924aa6c137277adccec74464
Miranda Cascione <miranda.cascione@gmail.com>
2023-06-12 13:13:38 -0700

    added handle change function for todo form inputs

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,13 +3,17 @@ import React, { useState } from "react";
 function TodoForm(props) {
   const [input, setInput] = useState("");
 
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    props.onSubmit({
-      id: Math.floor(Math.random() * 10000),
-      text: input,
-    });
+    // props.onSubmit({
+    //   id: Math.floor(Math.random() * 10000),
+    //   text: input,
+    // });
     setInput("");
   };
 
@@ -21,6 +25,7 @@ function TodoForm(props) {
         value={input}
         name="text"
         className="todo-input"
+        onChange={handleChange}
       />
       <button className="todo-button">Add Todo</button>
     </form>
